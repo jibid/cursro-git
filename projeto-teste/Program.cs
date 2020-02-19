126c7802ee85c42a89686c7437fefb422853f4f2
Jonas Hamerski <jonas@ibid.com.br>
2020-02-19 11:04:15 -0300

    Projeto iniciando mostrando a mensagem Ola

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Olá");
+        }
+    }
+}
